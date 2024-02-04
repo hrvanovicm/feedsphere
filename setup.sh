f10bb5d38a5bf73368b3fe5340cb81084986ced5
bytemike <hrvanovicmirza64@gmail.com>
2024-02-04 02:00:17 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 echo "Installing required packages for feedsphere"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sudo apt-get update
-  sudo apt-get install build-essential python-pip libffi-dev python-dev python3-dev libpq-dev
-else
-  echo "Please install better OS (e.g. Linux) :)"
-  exit
-fi
+sudo apt-get update
+sudo apt-get install build-essential python-pip libffi-dev python-dev python3-dev libpq-dev
 
 rm -rf .venv
 python -m venv .venv
