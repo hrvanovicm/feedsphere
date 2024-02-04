7f607346dbf4fc7b9155b075d84e1fc29fe5181f
bytemike <hrvanovicmirza64@gmail.com>
2024-02-04 01:45:10 +0100

    update setup
    - Break script if user is not using the best OS on the world Linux.
    - Add all packages to install for pip.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-echo "Installing required packages for Feedsphere"
+echo "Installing required packages for feedsphere"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt-get update
   sudo apt-get install build-essential python-pip libffi-dev python-dev python3-dev libpq-dev
+else
+  echo "Please install better OS (ex. Linux) :)"
+  exit
 fi
 
 rm -rf .venv
 python -m venv .venv
 source .venv/bin/activate
-pip install falcon gunicorn httpie
+pip install falcon gunicorn httpie cerbeus beatifulsoup4 feedparser alembic sqlalchemy bcrypt apscheduler
